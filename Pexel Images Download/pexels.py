b55df8f25c7a31001a1b41f550e916a2a70d9916
axeshay <akshayshambharkar31@gmail.com>
2017-12-15 10:10:54 +0530

    Create pexels.py

---
+++ Pexel Images Download/pexels.py
@@ -0,0 +1,18 @@
+import bs4 as bs
+import os
+import requests
+os.makedirs('pexels', exist_ok=True)
+user_input = str(input("Enter the search term: "))
+res = requests.get("https://pexels.com/search/" + user_input)
+soup = bs4.BeautifulSoup(res.text, "lxml")
+image_element = soup.select("article a img")
+for i in range(0, len(image_element)):
+    image_url = image_element[i].get('src').split("?")[0]
+    print("Downloading image %s ...." % (image_url))
+    image = requests.get(image_url)
+    image.raise_for_status()
+    imageFile = open(os.path.join('pexels',os.path.basename(image_url)),"wb")
+    for img in image.iter_content(1000000):
+        imageFile.write(img)
+    imageFile.close()
+print("Done")      
